3f89fbdfe773773a27e89618b53a024f184a7353
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-11-29 13:25:16 -0400

    Camera Movement
    Added camera movement to the system.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,12 +6,23 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
     [SerializeField] private MeshGenerator meshGenerator;
+    [SerializeField] private float moveTime = 1;
+    [SerializeField] private float vertMult = 3;
     [SerializeField][Range(0,1)] private float distanceBuffer;
     [SerializeField][Range(0,1)] private float heightBuffer;
+    private float zPos;
+    private float yPos;
+    private Vector3 destination;
+    private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {
-        
+        zPos = -meshGenerator.GetSize() * (1 + distanceBuffer);
+        yPos = meshGenerator.GetHeight() * (1 + heightBuffer) * vertMult;
+        destination = new Vector3(0f, yPos, zPos);
+
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, moveTime);
+        transform.LookAt(Vector3.zero);
     }
 }
